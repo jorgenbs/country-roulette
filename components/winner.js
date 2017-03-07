97d2b897c5ab103573c724b7978c25077e8f3dbc
Jørgen Bøe Svendsen <jorgenbs@gmail.com>
2017-03-07 18:08:06 +0100

    a typo

--- components/winner.js
+++ components/winner.js
@@ -4,7 +4,7 @@ const JSONPretty = React.createFactory(require('react-json-pretty'))
 module.exports = ({winner}) => {
   const header = React.createElement('h1', null, winner.name.common)
   const link = React.createElement('a',
-    {"href": `http://en.wikipedia.org/wiki/${winner.demonym}_Cuisine`}, 
+    {"href": `https://en.wikipedia.org/wiki/${winner.demonym}_cuisine`}, 
     `Go to wiki article about ${winner.demonym} cuisine`
   )
   return React.createElement('div', {className: 'winner-container'}, header, link)
