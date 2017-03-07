3315ab50dc17fbc9d9d3f1c5c631ca916008dd86
Jørgen Bøe Svendsen <jorgenbs@gmail.com>
2017-03-07 18:01:27 +0100

    changed cronjob time and added wiki-link

--- components/winner.js
+++ components/winner.js
@@ -3,5 +3,9 @@ const JSONPretty = React.createFactory(require('react-json-pretty'))
 
 module.exports = ({winner}) => {
   const header = React.createElement('h1', null, winner.name.common)
-  return React.createElement('div', {className: 'winner-container'}, header/*, JSONPretty({json: winner})*/)
+  const link = React.createElement('a',
+    {"href": `http://en.wikipedia.org/wiki/${winner.demonym}_Cuisine`}, 
+    `Go to wiki article about ${winner.demonym} cuisine`
+  )
+  return React.createElement('div', {className: 'winner-container'}, header, link)
 }
